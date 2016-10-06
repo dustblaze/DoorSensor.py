00a7fb88a10c04286a4821e0277f544bc6c98a5c
jayman555 <jay_hansen1997@yahoo.com.au>
2016-10-06 07:30:27 +0000

    removed key lelelel

--- DoorSensor.py
+++ DoorSensor.py
@@ -1,9 +1,9 @@
 __author__ = 'jayman555'
 
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import datetime
-import random
+#import random
 import os
 import subprocess
 
@@ -11,13 +11,14 @@ import subprocess
 statistics_fn = "door_stats.txt"
 door_state_fn = "door_state.txt"
 door_pin = 2
+poll_rate = 60.0 # seconds
 
 
 def start_recording():
     previous_state = "closed"
     while True:
         # Get the door state
-        door_state = bool(random.getrandbits(1)) # GPIO.input(door_pin)
+        door_state =  GPIO.input(door_pin) # bool(random.getrandbits(1)) # GPIO.input(door_pin)
         door_state = "open" if door_state else "closed"
 
         # Overwrite the door state in the file, only if its changed
@@ -27,10 +28,11 @@ def start_recording():
             door_state_file.close()
 
             # UPDATE THE WEBSITE
-            update_command = "curl -k https://cs.club.anu.edu.au/files/doorstate/set.php\?key\=PHO9nofEfXdUMLnu7ReSoRgpDgOTwgY\&state\={0}".format(door_state)
+            update_command = "censored haha nice try guy"
             # subprocess.call(['curl', '-k', update_command], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
             # subprocess.call(['curl', '-k', update_command])
             os.system(update_command)
+            # print(update_command)
 
             # Dump the door state to a file for analysis later
             timestamp = time.time()
@@ -42,9 +44,9 @@ def start_recording():
         previous_state = door_state
 
         # Try again later pal
-        time.sleep(5.0)
+        time.sleep(poll_rate)
 
 if __name__ == "__main__":
-    # GPIO.setmode(GPIO.BCM)
-    # GPIO.setup(2, GPIO.IN)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(2, GPIO.IN)
     start_recording()
